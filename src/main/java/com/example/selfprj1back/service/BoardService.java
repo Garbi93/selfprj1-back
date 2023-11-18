cd4f6dabd47efa6f3d8bd96dc78ea7e280b38cf2
Garbi93 <iseung2093@gmail.com>
2023-11-18 14:56:27 +0900

    11/18 mapper 에서 int타입으로 값을 받아 boolean 타입으로 반환

--- src/main/java/com/example/selfprj1back/service/BoardService.java
+++ src/main/java/com/example/selfprj1back/service/BoardService.java
@@ -11,7 +11,10 @@ public class BoardService {
 
     private final BoardMapper mapper;
 
-    public void save(Board board) {
-        mapper.insert(board);
+    public boolean save(Board board) {
+        // mapper가 return한 int 즉 값이 저장이 1개만 되면 1을 반환 하기 때문에 1을 받으면
+        // boolean 타입으로 true 를 controller에 전달하고,
+        // mapper 에서 1을 못 받으면 false 를 controller에 반환 한다.
+        return mapper.insert(board) == 1;
     }
 }
