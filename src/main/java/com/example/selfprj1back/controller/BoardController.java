b5b29047e3d079042edb5eb6461142c8293832ea
Garbi93 <iseung2093@gmail.com>
2023-11-18 13:59:52 +0900

    11/18 3tier Aci 로 개발 하기 위해 controller 에 service 호출

--- src/main/java/com/example/selfprj1back/controller/BoardController.java
+++ src/main/java/com/example/selfprj1back/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.selfprj1back.controller;
 
 import com.example.selfprj1back.domain.Board;
+import com.example.selfprj1back.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
 public class BoardController {
+    // board 서비스로 요청할 수 있도록 final 생성
+    private final BoardService service;
 
     @PostMapping("add")
     // json 으로 넘어온 값을 받는 @RequestBody 어노테이션을 써서 Board DTO에 저장 시키고 board 라는 변수로 사용 한다 .
     public void add(@RequestBody Board board) {
-        System.out.println("board = " + board);
+        // service에서 save 요청 사용
+        service.save(board);
     }
 }
