ae34e954b2da17d3623bff3b8a90be9e18abe504
Garbi93 <iseung2093@gmail.com>
2023-11-18 13:35:28 +0900

    11/18 spring Backend 프로젝트 생성

---
+++ src/main/java/com/example/selfprj1back/Selfprj1BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.selfprj1back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Selfprj1BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Selfprj1BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/selfprj1back/Selfprj1BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selfprj1back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Selfprj1BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
