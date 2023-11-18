a9bd37ef9a5c29c8da708be5e598966494780168
Garbi93 <iseung2093@gmail.com>
2023-11-18 13:45:19 +0900

    11/18 board DTO 작성

---
+++ src/main/java/com/example/selfprj1back/domain/Board.java
@@ -0,0 +1,11 @@
+package com.example.selfprj1back.domain;
+
+import lombok.Data;
+
+@Data
+public class Board {
+    private String writer;
+    private String title;
+    private String content;
+
+}
