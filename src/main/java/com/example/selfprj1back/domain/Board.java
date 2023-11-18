ab1a90ec35aa6ff8b2e9cf4801f3d0a1d5911665
Garbi93 <iseung2093@gmail.com>
2023-11-18 16:16:59 +0900

    11/18 조회 할때 필요한 dto내용을 추가로 작성

--- src/main/java/com/example/selfprj1back/domain/Board.java
+++ src/main/java/com/example/selfprj1back/domain/Board.java
@@ -2,10 +2,13 @@ package com.example.selfprj1back.domain;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class Board {
+    private Integer id;
     private String writer;
     private String title;
     private String content;
-
+    private LocalDateTime inserted;
 }
