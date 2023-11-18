bcd5e77b01ccfe2960e6ba2285d02e1290344aae
Garbi93 <iseung2093@gmail.com>
2023-11-18 13:47:18 +0900

    11/18 post요청에 add 시킬때 json 파일의 데이터를 받기위해 @RequestBody 어노테이션을 사용 하고 받은 json 파일을 Board Dto 에 저장 하여 board 라는 변수로 사용 하도록 코드 작성

--- src/main/java/com/example/selfprj1back/controller/BoardController.java
+++ src/main/java/com/example/selfprj1back/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.example.selfprj1back.controller;
 
+import com.example.selfprj1back.domain.Board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 
     @PostMapping("add")
-    public void add() {
-        System.out.println("BoardController.add");
+    // json 으로 넘어온 값을 받는 @RequestBody 어노테이션을 써서 Board DTO에 저장 시키고 board 라는 변수로 사용 한다 .
+    public void add(@RequestBody Board board) {
+        System.out.println("board = " + board);
     }
 }
